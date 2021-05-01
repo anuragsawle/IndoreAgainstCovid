544b87cfa859ae6af8f557926901c72daa3a7d99
anurag <anuragsawle70@gmail.com>
2021-05-01 16:03:31 +0530

    New Medicine added part added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 data = {'1': 'bed+OR+beds', '2': 'icu', '3': 'oxygen', '4': 'ventilator+OR+ventilators',
         '5': 'test+OR+tests+OR+testing', '6': 'remdesivir', '7': 'favipiravir',
-        '8': 'tocilizumab', '9': 'plasma', '10': 'food'}
+        '8': 'tocilizumab', '9': 'plasma', '10': 'food', '11': 'Fabiflu', '12': 'ambulance'}
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -23,7 +23,7 @@ def index():
     if request.method == 'POST':
         d = request.form['data']
         q = 'https://twitter.com/search?q=verified+indore+'
-        x = q + data[d] + '+-"needed"+-"required"&f=live'
+        x = q + data[d] + '+-"needed"+-"need" + -"needs"+-"required"+ -"require" + -"requires" + -"requirement" + -"requirements"&f=live'
         return redirect(x)
     return render_template("IndoreAgainstCovid.html")
 
